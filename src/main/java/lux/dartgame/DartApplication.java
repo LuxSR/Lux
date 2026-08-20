24639530bf5cd6fb29fae3185dffa84d230b3dcd
Riccardo <riccardo.ieva@no.experis.com>
2026-08-20 15:29:00 +0200

    chore: create layout and Readme with conventions.

---
+++ src/main/java/lux/dartgame/DartApplication.java
@@ -0,0 +1,8 @@
+package lux.dartgame;
+
+public class DartApplication {
+    // Todo: Implement application
+    public static void main(final String[] args) {
+        System.out.println("Starting Dart Game!");
+    }
+}
