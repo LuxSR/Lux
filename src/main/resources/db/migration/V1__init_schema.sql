d9b818002a715d61b43bd1ce83ea8e3184ef118d
Riccardo <riccardo.ieva@no.experis.com>
2026-09-04 08:16:26 +0200

    Ãrefactor: refactor auth and database files to match refactored entities.

--- src/main/resources/db/migration/V1__init_schema.sql
+++ src/main/resources/db/migration/V1__init_schema.sql
@@ -1,24 +1,34 @@
-DROP TABLE IF EXISTS users;
-DROP TABLE IF EXISTS gametypes;
-DROP TABLE IF EXISTS sessions;
-DROP TABLE IF EXISTS games;
+DROP TABLE IF EXISTS session_mm_users;
 DROP TABLE IF EXISTS game_mm_gametypes;
-DROP TABLE IF EXISTS playerstats;
 DROP TABLE IF EXISTS gamestats;
+DROP TABLE IF EXISTS playerstats;
+DROP TABLE IF EXISTS games;
+DROP TABLE IF EXISTS sessions;
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS roles;
+DROP TABLE IF EXISTS gametypes;
+
+CREATE TABLE roles (
+    role_id BIGSERIAL PRIMARY KEY,
+    role VARCHAR(255) NOT NULL UNIQUE
+);
 
 CREATE TABLE users (
     user_id BIGSERIAL PRIMARY KEY,
-    user_name VARCHAR(255) NOT NULL
+    user_name VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    role_id BIGINT NOT NULL REFERENCES roles(role_id)
 );
 
 CREATE TABLE gametypes (
     gametype_id BIGSERIAL PRIMARY KEY,
-    gametype VARCHAR(255) NOT NULL
+    gametype VARCHAR(255) NOT NULL UNIQUE
 );
 
 CREATE TABLE sessions (
     session_id BIGSERIAL PRIMARY KEY,
-    user_id BIGINT NOT NULL REFERENCES users(user_id)
+    is_active BOOLEAN NOT NULL,
+    owner_id BIGINT NOT NULL REFERENCES users(user_id)
 );
 
 CREATE TABLE games (
@@ -29,9 +39,15 @@ CREATE TABLE games (
 );
 
 CREATE TABLE game_mm_gametypes (
-    gameId BIGINT NOT NULL REFERENCES games(game_id),
-    gametypeId BIGINT NOT NULL REFERENCES gametypes(gametype_id),
-    PRIMARY KEY (gameId, gametypeId)
+    game_id BIGINT NOT NULL REFERENCES games(game_id),
+    gametype_id BIGINT NOT NULL REFERENCES gametypes(gametype_id),
+    PRIMARY KEY (game_id, gametype_id)
+);
+
+CREATE TABLE session_mm_users (
+    session_id BIGINT NOT NULL REFERENCES sessions(session_id),
+    user_id BIGINT NOT NULL REFERENCES users(user_id) ON DELETE CASCADE,
+    PRIMARY KEY (session_id, user_id)
 );
 
 CREATE TABLE playerstats (
@@ -42,15 +58,19 @@ CREATE TABLE playerstats (
     triple20s INTEGER,
     bullseyes INTEGER,
     highest_score INTEGER,
-    player_id BIGINT UNIQUE NOT NULL REFERENCES users(user_id)
+    highest_checkout INTEGER NOT NULL,
+    avg_checkout_accuracy REAL NOT NULL,
+    player_id BIGINT UNIQUE NOT NULL REFERENCES users(user_id) ON DELETE CASCADE
 );
 
 CREATE TABLE gamestats (
-    user_id BIGINT NOT NULL REFERENCES users(user_id),
+    user_id BIGINT NOT NULL REFERENCES users(user_id) ON DELETE CASCADE,
     game_id BIGINT NOT NULL REFERENCES games(game_id),
     avg_points REAL,
     triple20s INTEGER,
     bullseyes INTEGER,
     highest_score INTEGER,
+    checkout_accuracy REAL NOT NULL,
+    highest_checkout INTEGER NOT NULL,
     PRIMARY KEY (user_id, game_id)
 );
