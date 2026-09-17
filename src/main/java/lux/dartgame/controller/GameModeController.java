4f24c5dccc138adbac169225b69cd7beb690ef91
Riccardo <riccardo.ieva@no.experis.com>
2026-09-17 11:11:12 +0200

    refactor: change api endpoints to start with api.

--- src/main/java/lux/dartgame/controller/GameModeController.java
+++ src/main/java/lux/dartgame/controller/GameModeController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/gamemode")
+@RequestMapping("api/gamemode")
 public final class GameModeController {
     private final GametypeService gametypeService;
 
